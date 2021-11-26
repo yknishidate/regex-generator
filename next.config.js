c61a2b9fea630f6c84d617bc6cacfc7c86c377a6
nishidate-yuki <yuki18425@gmail.com>
2021-11-26 20:23:03 +0900

    update setting

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const urlPrefix = process.env.URL_PREFIX ? "/" + process.env.URL_PREFIX : "";
+
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
+};
