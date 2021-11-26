907ae5686850aa85e86761e79eaad23a3f8cf3a0
nishidate-yuki <yuki18425@gmail.com>
2021-11-26 20:29:27 +0900

    Updates

---
+++ _next/static/18Gpomig18BoSrXiR2LYA/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/832-03bb99c92aa4406a.js","static/css/a98b794046e4288f.css","static/chunks/pages/index-ac1ae04d4eb11bea.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
