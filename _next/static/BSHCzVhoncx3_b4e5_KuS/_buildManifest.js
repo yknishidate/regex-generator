8019e1e69f1a8c96ec90c7bc97f8f505c901307d
yknishidate <yknishidate@users.noreply.github.com>
2022-10-21 06:21:36 +0000

    deploy: 9a83bdda7710d6c39cbad96ff2199e9a7bd8143b

---
+++ _next/static/BSHCzVhoncx3_b4e5_KuS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/478-19509117136d6aee.js","static/css/f7228828040cf1b9.css","static/chunks/pages/index-1416eb529e7afc09.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
